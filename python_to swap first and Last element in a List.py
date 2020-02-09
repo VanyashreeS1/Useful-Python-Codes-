e87688e56ed0c9f7e45156a1402925c739c1e505
vanyashree <vanyashree.s@gmail.com>
2020-02-09 23:16:48 +0530

    python progrms practice1

--- python_to swap first and Last element in a List.py
+++ python_to swap first and Last element in a List.py
@@ -5,7 +5,8 @@ Python3 program to swap first
 def swapList(newList): 
     size = len(newList) 
       
-    # Swapping  
+    # Swapping 
+	
     temp = newList[0] 
     newList[0] = newList[size - 1] 
     newList[size - 1] = temp 
